08818c6d17d7125bbdb1cccf0f07fbb7bbfe5cee
Amaan Shaikh <amaan.shaikh@launchventures.co>
2022-07-18 12:32:20 +0530

    Final Change added some js files and changed css as well as html

---
+++ js/catalog_detail.js
@@ -0,0 +1,9 @@
+function Display() 
+{
+    var x = document.getElementById("panel");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
